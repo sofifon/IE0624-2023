69fb5a1c50948a559fc626fc59c82ca382ce6c07
Sofi <sofifon@emma.localdomain>
2023-02-28 14:36:40 -0500

    Creacion de archivo para deteccion de movimientos en la STM

---
+++ Lab5_AI/src/detection/gyro_publisher.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import serial, time, json, csv
+
+ser = serial.Serial(port = '/dev/ttyACM0', baudrate=115200) 
+print("Connected to MCU")
+file = open('datos.csv', 'w')
+writer = csv.writer(file)
+data_rows = []
+header = ['x', 'y', 'z']
+print(header)
+writer.writerow(header)
+
+while(1):
+    data = ser.readline().decode('utf-8').replace('\r', "").replace('\n', "")
+    data = data.split('\t')
+    if len(data) >= 1:
+        writer.writerow(data)
+        print(data)
+
+
+
